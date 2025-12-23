1d4a428be777cc500453e2b88589b8f2a9835d63
BUG killer：） <38523332+woyaodangrapper@users.noreply.github.com>
2025-12-23 18:04:40 +0800

    chore: add license acceptance and general library descriptor

--- sample/Dlt645/Program.cs
+++ sample/Dlt645/Program.cs
@@ -2,7 +2,6 @@
 
 using Aspdcs.Rtu.DLT645;
 //using Aspdcs.Rtu.DLT645.Extensions;
-using Aspdcs.Rtu.DLT645.Serialization;
 using Microsoft.Extensions.Logging;
 
 // 运行示例代码
@@ -14,34 +13,56 @@ var console = LoggerFactory.Create(builder =>
 });
 
 
-IDlt645Client client = ChannelOptions.CreateBuilder("MyChannel")
-    .WithChannel("COM5")
-    .WithAuto()
-    .Run();
-
-// 使用默认配置创建客户端
-//var options = ChannelOptions.CreateDefaultBuilder()
+//IDlt645Client client = ChannelOptions.CreateBuilder("MyChannel")
 //    .WithChannel("COM5")
-//    .Build();
+//    .WithAuto()
+//    .Run();
 
-//Dlt645Client client = new(options);
+//// 使用默认配置创建客户端
+////var options = ChannelOptions.CreateDefaultBuilder()
+////    .WithChannel("COM5")
+////    .Build();
+
+////Dlt645Client client = new(options);
 
 
-List<AddressValue> addresses = [];
-// 广播获取所有电表设备地址
-await foreach (var address in client.TryReadAddressAsync())
-{
-    addresses.Add(address);
-}
+//List<AddressValue> addresses = [];
+//// 广播获取所有电表设备地址
+//await foreach (var address in client.TryReadAddressAsync())
+//{
+//    addresses.Add(address);
+//}
 
-// 读取所有电表总(正向有功)电量数据
-await foreach (var frame in client.ReadAsync("111100000000"))
-{
-    Console.WriteLine($"Received: {frame}");
-}
+//// 读取所有电表总(正向有功)电量数据
+//await foreach (var frame in client.ReadAsync("111100000000"))
+//{
+//    Console.WriteLine($"Received: {frame}");
+//}
 //// 读取 1997 版电表总(正向有功)电量数据
 //await foreach (var frame in client.Read1997Async("111100000000"))
 //{
 //    Console.WriteLine($"Received: {frame}");
 //}
+
+{
+    var client = ChannelOptions.CreateDefaultBuilder()
+        .WithChannel("COM5")
+        .Run();
+
+    uint[] dataItems =
+    [
+        0x00010000,  // 正向有功总电能
+        0x02010100,  // A 相电压
+        0x02020100   // A 相电流
+    ];
+
+    foreach (var dataId in dataItems)
+    {
+        await foreach (var frame in client.ReadAsync("111100000000", dataId))
+        {
+            Console.WriteLine($"数据标识 {dataId:X8}: {frame}");
+        }
+    }
+
+}
 Console.WriteLine("Hello, World!");
